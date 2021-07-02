6e2f0ded8a23b7198f15ea8236305a0c86b21446
Sean Andrews <sandrews2332@gmail.com>
2021-07-02 09:04:42 -0400

    Create function for random computer choice

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello World");
+// This function gives the computer the random assignment of rock, paper, or scissors //
+
+    // Function generates integers 0, 1, or 2 randomly
+
+function computerPlay() {
+    let num = Math.floor(Math.random() * 3);
+    
+    //  Function then assigns each integer either rock, paper, or scissors 
+    
+    if (num === 0) {
+        console.log("Rock!");
+        return rock;
+    } else if (num === 1) {
+        console.log("Paper!");
+        return paper;
+    } else {
+        console.log("Scissors!")
+        return scissors;
+    }
+}
+
+computerPlay()
